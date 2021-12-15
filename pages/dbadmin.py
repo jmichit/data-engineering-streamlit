202065cd34613220c38b9635dbf303cfd019ed10
John Michitsch <jmichit@yahoo.com>
2021-12-15 17:53:48 -0500

    initial

---
+++ pages/dbadmin.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+import pandas as pd
+import datetime
+
+from StockDB import StockDB 
+from Accounts import Account
+
+
+def dbadmin:
+
+    st.title('DBAdmin')
+
+    db = StockDB()
+    
+    table = db.db_summary()
+
+    db.close()
+
+    st.write(table)
